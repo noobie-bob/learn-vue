be06b95448413d22737c6c12354dd2197d532c82
mny <yashas.m-n@hpe.com>
2026-02-28 20:35:55 +0530

    initial commit with reset,add, sorting feature

--- vue-fundamentals-composition-api/src/App.vue
+++ vue-fundamentals-composition-api/src/App.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const msg = ref("Hello World")
+const header = ref('Shopping List App')
+const items = ref(['item 1', 'item 4', 'item 3', 'item 2'])
+const reorderItems = () => {
+  items.value.sort()
+}
+const resetItems = () => {
+  items.value = ['item 1', 'item 4', 'item 3', 'item 2']
+}
+const addItem = (item: string) => {
+  items.value.push(item)
+}
 </script>
 
 <template>
-  <h1>Uppercase :{{ msg.toLocaleUpperCase() }}</h1>
-  <h1>Lowercase :{{ msg.toLowerCase() }}</h1>
-  <h1>If-else-ternary: {{ msg ? msg : "Yo type something" }}</h1>
-  <h1>OR check: {{ msg || "Yo type something" }}</h1>
-  <input v-model="msg">
-  <input v-model="msg" class="padding-2">
+  <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      <input v-model="item" />
+    </li>
+  </ul>
+  <input type="text" placeholder="Add an item" @keyup.enter="
+      if ($event.target) {
+    addItem(($event.target as HTMLInputElement).value)
+      ; ($event.target as HTMLInputElement).value = ''
+  }
+    " />
+  <button class="padding-2" @click="reorderItems">Reorder Items</button>
+  <button class="padding-2" @click="resetItems">Reset Items</button>
 </template>
 
 <style scoped>
