a3dd8ba8475c8788471499025b4acc19a3265c5d
mny <yashas.m-n@hpe.com>
2026-02-28 13:15:47 +0530

    Testing if-else checks

--- vue-fundamentals-composition-api/src/App.vue
+++ vue-fundamentals-composition-api/src/App.vue
@@ -4,8 +4,16 @@ const msg = ref("Hello World")
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>Uppercase :{{ msg.toLocaleUpperCase() }}</h1>
+  <h1>Lowercase :{{ msg.toLowerCase() }}</h1>
+  <h1>If-else-ternary: {{ msg ? msg : "Yo type something" }}</h1>
+  <h1>OR check: {{ msg || "Yo type something" }}</h1>
   <input v-model="msg">
-  &nbsp;
-  <input v-model="msg">
+  <input v-model="msg" class="padding-2">
 </template>
+
+<style scoped>
+.padding-2 {
+  margin-left: 1rem;
+}
+</style>
